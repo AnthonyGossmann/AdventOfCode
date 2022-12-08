4f0cc47eabb68175076312c0c7b9d641d48c2cd0
Anthony Gossmann <119435789+LokeNox@users.noreply.github.com>
2022-12-08 10:08:29 +0100

    2022 day 8 V2

--- 2022/Puzzle8.py
+++ 2022/Puzzle8.py
@@ -9,48 +9,48 @@ from itertools import product
 # Static Methods
 ############################################################
 def isVisible(trees_, x_: int, y_: int) -> bool:
-    if ((x_ == 1) or (x_ == len(trees_) - 2) or (y_ == 1) or (y_ == len(trees_[0]) - 2)):
+    if ((x_ == 0) or (x_ == len(trees_)-1) or (y_ == 0) or (y_ == len(trees_[0])-1)):
         return True
     
     # Top
-    b1 = (trees_[x_,y_] > max(trees_[0:x_,y_]))
+    b1 = (trees_[x_,y_] > max(trees_[:x_,y_]))
     # Bottom
     b2 = (trees_[x_,y_] > max(trees_[x_+1:,y_]))
     # Left
-    b3 = (trees_[x_,y_] > max(trees_[x_,0:y_]))
+    b3 = (trees_[x_,y_] > max(trees_[x_,:y_]))
     # Right
     b4 = (trees_[x_,y_] > max(trees_[x_,y_+1:]))
 
     return (b1 or b2 or b3 or b4)
 
 def distance(trees_, x_: int, y_: int) -> int:
-    if ((x_ == 1) or (x_ == len(trees_) - 2) or (y_ == 1) or (y_ == len(trees_[0]) - 2)):
+    if ((x_ == 0) or (x_ == len(trees_)-1) or (y_ == 0) or (y_ == len(trees_[0])-1)):
         return 0
     
     # Top
     s1 = 0
-    for i in range(x_-1, 0, -1):
+    for i in range(x_-1, -1, -1):
         s1 += 1
         if (trees_[i,y_] >= trees_[x_,y_]):
             break
 
     # Bottom
     s2 = 0
-    for i in range(x_+1, len(trees_)-1, 1):
+    for i in range(x_+1, len(trees_), 1):
         s2 += 1
         if (trees_[i,y_] >= trees_[x_,y_]):
             break
 
     # Left
     s3 = 0
-    for i in range(y_-1, 0, -1):
+    for i in range(y_-1, -1, -1):
         s3 += 1
         if (trees_[x_,i] >= trees_[x_,y_]):
             break
     
     # Right
     s4 = 0
-    for i in range(y_+1, len(trees_[0])-1 , 1):
+    for i in range(y_+1, len(trees_[0]) , 1):
         s4 += 1
         if (trees_[x_,i] >= trees_[x_,y_]):
             break
@@ -77,21 +77,18 @@ class Puzzle8:
         length = len(lines)
         width = len(lines[0])
         
-        trees = np.zeros((length + 2, width + 2))
+        trees = np.zeros((length, width))
         trees.astype(int)
-        for i,j in product(range(length + 2), range(width + 2)):
-            if ((i == 0) or (i == length + 1) or (j == 0) or (j == width + 1)):
-                trees[i][j] = int(0)
-            else:
-                trees[i][j] = int(lines[i - 1][j - 1])
+        for i,j in product(range(length), range(width)):
+           trees[i][j] = int(lines[i][j])
             
         # Part 1
-        for i,j in product(range(1, length + 1), range(1, width + 1)):
+        for i,j in product(range(length), range(width)):
             if isVisible(trees, i, j):
                 self.result1 += 1
 
         # Part 2
-        for i,j in product(range(1, length + 1), range(1, width + 1)):
+        for i,j in product(range(length), range(width)):
             score = distance(trees, i, j)
             self.result2 = max(self.result2, score)
         
