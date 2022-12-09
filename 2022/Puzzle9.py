d97cc8957c0c50697c9c2e5aaedf5c1235805896
Anthony Gossmann <119435789+AnthonyGossmann@users.noreply.github.com>
2022-12-09 10:08:10 +0100

    2022 day 9 V2

--- 2022/Puzzle9.py
+++ 2022/Puzzle9.py
@@ -76,77 +76,58 @@ class Puzzle9:
     
     def move(self, head_: Point, tail_: Point) -> Point:        
         if (tail_ not in self.getNeighbours(head_)):
-            # Up
-            if ((tail_.x == head_.x) and (head_.y > tail_.y)):
-                tail_.y += 1
-            # Down
-            elif ((tail_.x == head_.x) and (head_.y < tail_.y)):
-                tail_.y -= 1
-            # Right
-            elif ((tail_.y == head_.y) and (head_.x > tail_.x)):
-                tail_.x += 1
-            # Left
-            elif ((tail_.y == head_.y) and (head_.x < tail_.x)):
-                tail_.x -= 1
-            # Up Right
-            elif ((head_.x > tail_.x) and (head_.y > tail_.y)):
-                tail_.x += 1
-                tail_.y += 1
-            # Down Right
-            elif ((head_.x > tail_.x) and (head_.y < tail_.y)):
-                tail_.x += 1
-                tail_.y -= 1
-            # Up Left
-            elif ((head_.x < tail_.x) and (head_.y > tail_.y)):
-                tail_.x -= 1
-                tail_.y += 1
-            # Down Left
-            else:
-                tail_.x -= 1
-                tail_.y -= 1
+            dx = (head_.x - tail_.x)
+            dy = (head_.y - tail_.y)
+            
+            if (dx < -1):
+                dx = -1
+            elif (dx > 1):
+                dx = 1
+            
+            if (dy < -1):
+                dy = -1
+            elif (dy > 1):
+                dy = 1
+                
+            tail_.x += dx
+            tail_.y += dy
                         
         return tail_
     
-    def move10(self, head_: Point, ropes_: List[Point], cmd_: str):
-        ropes = []
-        head_ = self.moveHead(head_, cmd_)
-        h = head_
+    def move10(self, ropes_: List[Point], cmd_: str) -> List[Point]:
+        ropes_[0] = self.moveHead(ropes_[0], cmd_)
         
-        for i in range(9):
-            t = ropes_[i]
-            h = self.move(h, t)          
-            ropes.append(h)
+        for i in range(1, 10):
+            ropes_[i] = self.move(ropes_[i-1], ropes_[i])          
             
-        return [head_, ropes]
+        return ropes_
             
 
     def run(self):
         lines = readLines(self.filename)
 
         # Part 1
-        head = Point(0,0)
-        tail = Point(0,0)
+        ropes = [Point(0,0), Point(0,0)]
         points = [Point(0,0)]
         
         for line in lines:
             tokens = line.split(" ")
             for i in range(int(tokens[1])):
-                head = self.moveHead(head, tokens[0])
-                tail = self.move(head, tail)
-                if tail not in points:
-                    points.append(Point(tail.x,tail.y))
+                ropes[0] = self.moveHead(ropes[0], tokens[0])
+                ropes[1] = self.move(ropes[0], ropes[1])
+                if ropes[1] not in points:
+                    points.append(Point(ropes[1].x,ropes[1].y))
             
         self.result1 = len(points)
         
         # Part 2
-        head = Point(0,0)
-        ropes = [Point(0,0) for i in range(9)]
+        ropes = [Point(0,0) for i in range(10)]
         points = [Point(0,0)]
         
         for line in lines:
             tokens = line.split(" ")
             for i in range(int(tokens[1])):
-                [head, ropes] = self.move10(head, ropes, tokens[0])
+                ropes = self.move10(ropes, tokens[0])
                 if ropes[-1] not in points:
                     points.append(Point(ropes[-1].x,ropes[-1].y))
 
