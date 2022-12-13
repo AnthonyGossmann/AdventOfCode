37a2d7ca066cb0af0050d0de6848670231d59ec3
Anthony Gossmann <119435789+AnthonyGossmann@users.noreply.github.com>
2022-12-13 14:56:03 +0100

    2022 day 13 v2

--- 2022/Puzzle13.py
+++ 2022/Puzzle13.py
@@ -10,87 +10,18 @@ import functools
 ############################################################
 # Methods
 ############################################################
-def getNextClosingBrackets(text_: str, start_: int):
-    counter = 0
-    for i in range(start_, len(text_)):
-        if (text_[i] == '['):
-            counter += 1
-        elif (text_[i] == ']'):
-            counter -= 1
-            if (counter == 0):
-                return i
-    raise Exception("Closing bracket not found.")
-
-def splitOutsideBrackets(text_: str):
-    tokens = []
-    counter = 0
-    last = 0
-    
-    for i in range(len(text_)):
-        if (text_[i] == '['):
-            counter += 1
-        elif (text_[i] == ']'):
-            counter -= 1
+def compare(l1_, l2_):
+    for i in range(min(len(l1_), len(l2_))):
+        if ((type(l1_[i]) == int) and (type(l2_[i]) == int)):
+            if (l1_[i] == l2_[i]):
+                continue
+            return (l1_[i] - l2_[i])
         
-        if ((text_[i] == ',') and (counter == 0)):
-            tokens.append(text_[last:i])
-            last = i+1
-    tokens.append(text_[last:])
-    
-    return tokens
-    
-def getItems(text_: str):
-    text = text_
-    if text.startswith('['):
-        close = getNextClosingBrackets(text, 0)
-        text = text[1:close]
-    
-    items = splitOutsideBrackets(text)
-    
-    return items
-
-def compare(l1_: str, l2_: str) -> int:
-    #print("Compare " + l1_ + " vs " + l2_)
-    
-    # Empty items
-    if (l1_ == "[]") and (l2_ == "[]"):
-        return 0
-    elif (l1_ == "[]"):
-        return 1
-    elif (l2_ == "[]"):
-        return -1
-    
-    # Mixed types
-    if (l1_.startswith('[') and not l2_.startswith('[')):
-        l2_ = '[' + l2_ + ']'
-    if (l2_.startswith('[') and not l1_.startswith('[')):
-        l1_ = '[' + l1_ + ']'
-        
-    # Compare items
-    if (('[' not in l1_) and ('[' not in l2_)):
-        if (int(l1_) == int(l2_)):
-            return 0
-        elif (int(l1_) < int(l2_)):
-            return 1
-        else:
-            return -1
-
-    # Children
-    i1 = getItems(l1_)
-    i2 = getItems(l2_)
-    
-    for i in range(min(len(i1), len(i2))):
-        res = compare(i1[i], i2[i])
-        if (res != 0):
+        res = compare(l1_[i] if (type(l1_[i]) == list) else [l1_[i]], l2_[i] if (type(l2_[i]) == list) else [l2_[i]])
+        if res:
             return res
     
-    # Ran out of items
-    if (len(i1) < len(i2)):
-        return 1
-    elif (len(i1) > len(i2)):
-        return -1
-
-    return 0
+    return (len(l1_) - len(l2_))
 
 ############################################################
 # Class Puzzle13
@@ -113,22 +44,20 @@ class Puzzle13:
 
     def run(self):
         lines = readLines(self.filename)
-        lines = [line for line in lines if len(line) != 0]
+        lines = [eval(line) for line in lines if len(line) != 0]
         
         # Part 1
         for i in range(0, len(lines)-1, 2):
             res = compare(lines[i], lines[i+1])
-            if (res == 1):
+            if (res < 0):
                 self.result1 += (i // 2) + 1
     
         # Part 2
-        lines.append("[[2]]")
-        lines.append("[[6]]")
+        lines.append(eval("[[2]]"))
+        lines.append(eval("[[6]]"))
         
         sorted_lines = sorted(lines, key=functools.cmp_to_key(compare))
-        sorted_lines = sorted_lines[::-1]
-        
-        self.result2 = (sorted_lines.index("[[2]]") + 1) * (sorted_lines.index("[[6]]") + 1)
+        self.result2 = (sorted_lines.index(eval("[[2]]")) + 1) * (sorted_lines.index(eval("[[6]]")) + 1)
             
         
         return            
